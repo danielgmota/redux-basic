76730094a73dad22dfcc055b47577383204c334e
Daniel Mota <danielgmrs@outlook.com>
2023-08-20 18:06:25 -0300

    feat: added changeColor reducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import ChangeColor from './components/ChangeColor';
 import Login from './components/Login';
 import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="App">
-      <Profile/>
-      <Login/>
+      <Profile />
+      <Login />
+      <ChangeColor />
     </div>
   );
 }
---
+++ src/components/ChangeColor.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { changeColor } from '../features/theme';
+
+function ChangeColor() {
+  const [color, setColor] = React.useState('');
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <input type="text" onChange={(ev) => setColor(ev.target.value)} />
+      <button onClick={() => dispatch(changeColor(color))}>Change Color</button>
+    </div>
+  );
+}
+
+export default ChangeColor;
